9ed94ec88de989b4224e2ff80430ebb58db43416
kyaputun10-tech <kyaputun.10@gmail.com>
2026-07-15 12:05:47 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,16 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is running"
+
+def run():
+    app.run(host='0.0.0.0', port=10000)
+
+Thread(target=run).start()
 import os
 import json
 import gspread
